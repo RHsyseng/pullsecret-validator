e415332888ef8709cf81bbb914d1457133b1dbea
Alberto Morgante <alknopfler@gmail.com>
2021-07-06 17:39:12 +0200

    WIP validator with curl

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/RHsyseng/pullsecret-validator
 
 go 1.16
+
+require (
+	docker.io/go-docker v1.0.0 // indirect
+	github.com/docker/docker v20.10.7+incompatible // indirect
+	github.com/docker/engine-api v0.4.0 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+)
--- validator/validator.go
+++ validator/validator.go
@@ -1,8 +1,11 @@
 package validator
 
 import (
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
 )
 
 const (
@@ -10,8 +13,15 @@ const (
 	ERROR = "error"
 )
 
+var (
+	payload          Payload
+	resultOKArray    []byte
+	resultKOArray    []byte
+	resultKOConArray []byte
+)
+
 func Validate(input []byte) WebData {
-	var payload Payload
+
 	err := json.Unmarshal(input, &payload)
 	if err != nil {
 		fmt.Println(ERROR, err)
@@ -21,7 +31,32 @@ func Validate(input []byte) WebData {
 		//fmt.Println("Esto es una key: ", k)
 		//fmt.Println("y esto su valor: ", v.Auth)
 		//fmt.Println("y esto su valor: ", v.Auth)
-		return WebData{input, k, v, k}
+		sDec, _ := b64.StdEncoding.DecodeString(v.Auth)
+		auth := string(sDec)
+		err, _ := loginToRegistry(k, auth)
+		if err != nil {
+			resultKOArray = append(resultKOArray, k+"\n"...)
+		}
+		resultOKArray = append(resultOKArray, k+"\n"...)
+
 	}
-	return WebData{}
+
+	return WebData{input, string(resultOKArray), string(resultKOArray), string(resultKOConArray)}
+}
+
+func loginToRegistry(url, auth string) (err error, bodyError string) {
+
+	req, err := http.NewRequest("GET", "https://"+url+"/v2/auth", nil)
+	if err != nil {
+		// handle err
+	}
+	s := strings.Split(auth, ":")
+	req.SetBasicAuth(s[0], s[1])
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err, resp.Status
+	}
+	defer resp.Body.Close()
+	return nil, ""
 }
