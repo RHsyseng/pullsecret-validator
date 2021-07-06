8c60260e9efacef73571a8addb44e26cd5867fbb
Alberto Morgante <alknopfler@gmail.com>
2021-07-06 18:06:08 +0200

    WIP validator with curl

--- validator/validator.go
+++ validator/validator.go
@@ -9,8 +9,9 @@ import (
 )
 
 const (
-	VALID = "valid"
-	ERROR = "error"
+	VALID      = "valid"
+	ERROR      = "error"
+	CONN_ERROR = "no such host"
 )
 
 var (
@@ -33,31 +34,32 @@ func Validate(input []byte) WebData {
 		//fmt.Println("y esto su valor: ", v.Auth)
 		sDec, _ := b64.StdEncoding.DecodeString(v.Auth)
 		auth := string(sDec)
-		err, _ := loginToRegistry(k, auth)
-		if err != nil {
-			resultKOArray = append(resultKOArray, k+"\n"...)
-
-		} else {
+		err := loginToRegistry(k, auth)
+		if err == nil && strings.Contains(err.Error(), CONN_ERROR) {
 			resultOKArray = append(resultOKArray, k+"\n"...)
+		} else if err != nil && strings.Contains(err.Error(), CONN_ERROR) {
+			resultKOConArray = append(resultKOConArray, k+"\n"...)
+		} else {
+			resultKOArray = append(resultKOArray, k+"\n"...)
 		}
 	}
 
 	return WebData{input, string(resultOKArray), string(resultKOArray), string(resultKOConArray)}
 }
 
-func loginToRegistry(url, auth string) (err error, bodyError string) {
+func loginToRegistry(url, auth string) error {
 
 	req, err := http.NewRequest("GET", "https://"+url+"/v2/auth", nil)
 	if err != nil {
-		return err, "Error doing request"
+		return err
 	}
 	s := strings.Split(auth, ":")
 	req.SetBasicAuth(s[0], s[1])
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err, resp.Status
+		return err
 	}
 	defer resp.Body.Close()
-	return nil, ""
+	return nil
 }
