de7b9ae1265f767bb7092932a1dfc4bb57877c4f
Alberto Morgante <alknopfler@gmail.com>
2021-07-07 15:37:59 +0200

    publishing to ocp internal

--- webmodule/webmodule.go
+++ webmodule/webmodule.go
@@ -17,6 +17,7 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		body := v.WebData{"Input your pull secret in json format", nil, nil, nil}
 		t, err := template.ParseFiles("./webmodule/web.html")
+		fmt.Println(os.Getwd())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusNotFound)
 			return
