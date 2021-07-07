03cd45d2349467a7d5f702c0b05eb9d4a9d33202
Alberto Morgante <alknopfler@gmail.com>
2021-07-07 12:39:20 +0200

    refactoring to a lib

--- webmodule/js/scripts.js
+++ webmodule/js/scripts.js
@@ -1,1 +1,25 @@
 // Empty JS for your own code to be here
+/*
+$(document).ready(function () {
+    $('#sendData').onsubmit(function (ev) {
+        ev.preventDefault();
+
+        $('#waiting').html('sending...');
+
+        $.ajax({
+            url: '/',
+            type: 'post',
+            enctype: 'multipart/form-data',
+            timeout: 800000,
+            data: $('#formValidator').serialize(),
+            onsuccess: function (data) {
+                console.log(data);
+                $('#waiting').html('sent!!!!!');
+            },
+            onerror: function (data) {
+                $('#waiting').html('error!! ' + data);
+            }
+        })
+    });
+})
+*/
